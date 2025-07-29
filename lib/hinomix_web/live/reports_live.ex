d87138084611577009bb7883fb6368e31054ea07
ntwali <ntwalihatsor78.nh@gmail.com>
2025-07-29 13:37:47 +0200

    add missing table fields

--- lib/hinomix_web/live/reports_live.ex
+++ lib/hinomix_web/live/reports_live.ex
@@ -12,18 +12,22 @@ defmodule HinomixWeb.ReportsLive do
     ~H"""
     <table border="1">
       <thead>
+        <th>Report ID</th>
         <th>Campaign ID</th>
         <th>Source</th>
         <th>Total clicks</th>
         <th>Total revenue</th>
+        <th>Report date</th>
       </thead>
       <tbody>
         <%= for report <- @reports do %>
           <tr>
+            <td>{report.report_id}</td>
             <td>{report.campaign_id}</td>
             <td>{String.capitalize(report.source)}</td>
             <td>{report.total_clicks}</td>
             <td>{report.total_revenue}</td>
+            <td>{report.report_date}</td>
           </tr>
         <% end %>
       </tbody>
